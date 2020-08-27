80c93e64e7f66031dd396f9d90c6658cb3128dfe
Staś Małolepszy <stas@piesku.com>
2020-08-27 09:05:48 +0200

    sys_physics in all 3 dimensions

--- src/components/com_rigid_body.ts
+++ src/components/com_rigid_body.ts
@@ -1,10 +1,13 @@
+import {Vec3} from "../../common/math.js";
 import {Entity, Game} from "../game.js";
 import {Has} from "../world.js";
 
+// Assume mass = 1, which means that acceleration and force are numerically equal.
+
 export interface RigidBody {
     readonly Dynamic: boolean;
-    VelY: number;
-    AccY: number;
+    Acceleration: Vec3;
+    Velocity: Vec3;
 }
 
 export function rigid_body(Dynamic: boolean = true) {
@@ -12,8 +15,8 @@ export function rigid_body(Dynamic: boolean = true) {
         game.World.Signature[entity] |= Has.RigidBody;
         game.World.RigidBody[entity] = {
             Dynamic,
-            VelY: 0,
-            AccY: 0,
+            Acceleration: [0, 0, 0],
+            Velocity: [0, 0, 0],
         };
     };
 }
--- src/systems/sys_physics.ts
+++ src/systems/sys_physics.ts
@@ -1,4 +1,5 @@
-import {add} from "../../common/vec3.js";
+import {Vec3} from "../../common/math.js";
+import {add, copy, scale} from "../../common/vec3.js";
 import {Entity, Game} from "../game.js";
 import {Has} from "../world.js";
 
@@ -20,10 +21,19 @@ function update(game: Game, entity: Entity, delta: number) {
 
     if (rigid_body.Dynamic) {
         transform.Dirty = true;
-        transform.Translation[1] += rigid_body.VelY * delta;
-        rigid_body.VelY += GRAVITY * delta;
-        rigid_body.VelY += rigid_body.AccY * delta;
-        rigid_body.AccY = 0;
+
+        // Compute change to velocity, including the gravity.
+        scale(rigid_body.Acceleration, rigid_body.Acceleration, delta);
+        add(rigid_body.Velocity, rigid_body.Velocity, rigid_body.Acceleration);
+        rigid_body.Velocity[1] += GRAVITY * delta;
+
+        // Apply velocity to position.
+        let vel_delta: Vec3 = [0, 0, 0];
+        scale(vel_delta, rigid_body.Velocity, delta);
+        add(transform.Translation, transform.Translation, vel_delta);
+
+        // Reset force/acceleration.
+        scale(rigid_body.Acceleration, rigid_body.Acceleration, 0);
 
         for (let i = 0; i < collide.Collisions.length; i++) {
             let collision = collide.Collisions[i];
@@ -33,13 +43,23 @@ function update(game: Game, entity: Entity, delta: number) {
                 // conversion to the collision response. Local space is world space.
                 add(transform.Translation, transform.Translation, collision.Hit);
 
+                // Assume mass = 1 for all rigid bodies. On collision,
+                // velocities are swapped, unless the other body is a static
+                // one (and behaves as if it had infinite mass).
+                let other_body = game.World.RigidBody[collision.Other];
+                let this_velocity = rigid_body.Velocity;
+                copy(rigid_body.Velocity, other_body.Velocity);
+                if (other_body.Dynamic) {
+                    copy(other_body.Velocity, this_velocity);
+                }
+
                 if (
                     // The rigid body was falling and hit something below it.
-                    (collision.Hit[1] > 0 && rigid_body.VelY < 0) ||
+                    (collision.Hit[1] > 0 && rigid_body.Velocity[1] < 0) ||
                     // The rigid body was going up and hit something above it.
-                    (collision.Hit[1] < 0 && rigid_body.VelY > 0)
+                    (collision.Hit[1] < 0 && rigid_body.Velocity[1] > 0)
                 ) {
-                    rigid_body.VelY = 0;
+                    rigid_body.Velocity[1] = 0;
                 }
             }
         }
