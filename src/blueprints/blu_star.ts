3e2ab7254d0d110d36fa3e78fc9c69ee5d3178fb
Staś Małolepszy <stas@piesku.com>
2020-08-29 12:14:40 +0200

    Textured star building

--- src/blueprints/blu_star.ts
+++ src/blueprints/blu_star.ts
@@ -1,6 +1,6 @@
 import {from_euler} from "../../common/quat.js";
 import {collide} from "../components/com_collide.js";
-import {render_colored_diffuse} from "../components/com_render_colored_diffuse.js";
+import {render_textured_diffuse} from "../components/com_render_textured_diffuse.js";
 import {RigidKind, rigid_body} from "../components/com_rigid_body.js";
 import {Blueprint} from "../core.js";
 import {Game, Layer} from "../game.js";
@@ -10,13 +10,22 @@ export function blueprint_star(game: Game, c: number): Blueprint {
         Using: [
             collide(true, Layer.Terrain, Layer.Player | Layer.Terrain),
             rigid_body(RigidKind.Dynamic),
-            render_colored_diffuse(game.MaterialDiffuseToon, game.MeshCube, [c, c, c, 1]),
+            render_textured_diffuse(
+                game.MaterialTexturedDiffuse,
+                game.MeshCube,
+                game.Textures["building2"]
+            ),
         ],
         Children: [
             {
+                Translation: [0, -0.1, 0],
                 Rotation: from_euler([0, 0, 0, 0], 0, 45, 0),
                 Using: [
-                    render_colored_diffuse(game.MaterialDiffuseToon, game.MeshCube, [c, c, c, 1]),
+                    render_textured_diffuse(
+                        game.MaterialTexturedDiffuse,
+                        game.MeshCube,
+                        game.Textures["building2"]
+                    ),
                 ],
             },
         ],
