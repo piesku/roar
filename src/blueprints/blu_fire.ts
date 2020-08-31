9c4fe4bf6e5c1f264ab7b06405ec567e35b993a1
Staś Małolepszy <stas@piesku.com>
2020-08-31 21:00:35 +0200

    More dense fire particles

--- src/blueprints/blu_fire.ts
+++ src/blueprints/blu_fire.ts
@@ -1,3 +1,4 @@
+import {from_euler} from "../../common/quat.js";
 import {cull} from "../components/com_cull.js";
 import {emit_particles} from "../components/com_emit_particles.js";
 import {render_particles} from "../components/com_render_particles.js";
@@ -8,12 +9,12 @@ import {Has} from "../world.js";
 
 export function blueprint_fire(game: Game): Blueprint {
     return {
-        Rotation: [-0.7071, 0, 0, 0.7071],
+        Rotation: from_euler([0, 0, 0, 0], -80, 10, 0),
         Children: [
             {
                 Using: [
                     shake(Infinity, 0.5),
-                    emit_particles(5, 0.1, 1, true),
+                    emit_particles(5, 0.05, 1, true),
                     render_particles(game.Textures["fire"], [1, 0.5, 0, 0.1], 50, [1, 0, 0, 0], 10),
                     cull(Has.Shake | Has.EmitParticles | Has.Render),
                 ],
