88aee6adfa4180aec768f43aaf17e0b31c95f3a3
Staś Małolepszy <stas@piesku.com>
2020-09-02 11:40:31 +0200

    Reduce the attack and release of sfx

--- src/sounds/snd_siren.ts
+++ src/sounds/snd_siren.ts
@@ -12,7 +12,7 @@ export let snd_siren: AudioClip = {
                 "square",
                 3,
                 3,
-                [["square", 8, 8, 11, 12, 7, true, false, 0, 0, 0]],
+                [["square", 8, 5, 18, 10, 7, true, false, 0, 0, 0]],
             ],
             Notes: [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76],
             Randomize: true,
