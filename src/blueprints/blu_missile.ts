9a7a1453c159b70efddb8c609c4ead35b34bff8f
Staś Małolepszy <stas@piesku.com>
2020-09-02 09:39:01 +0200

    Missile textures

--- src/blueprints/blu_missile.ts
+++ src/blueprints/blu_missile.ts
@@ -1,12 +1,12 @@
+import {from_euler} from "../../common/quat.js";
 import {float} from "../../common/random.js";
-import {GL_CW} from "../../common/webgl.js";
 import {control_move} from "../components/com_control_move.js";
 import {emit_particles} from "../components/com_emit_particles.js";
 import {lifespan} from "../components/com_lifespan.js";
 import {light_point} from "../components/com_light.js";
 import {move} from "../components/com_move.js";
 import {render_particles} from "../components/com_render_particles.js";
-import {render_textured_unlit} from "../components/com_render_textured_unlit.js";
+import {render_textured_diffuse} from "../components/com_render_textured_diffuse.js";
 import {shake} from "../components/com_shake.js";
 import {Blueprint} from "../core.js";
 import {Game} from "../game.js";
@@ -17,16 +17,15 @@ export function blueprint_missile(game: Game): Blueprint {
         Children: [
             {
                 // Body.
-                Scale: [0.2, 0.2, 0.5],
+                Rotation: from_euler([0, 0, 0, 0], 90, 0, 0),
+                Scale: [0.1, 1, 0.1],
                 Using: [
                     control_move(null, [0, 0, 1, 0]),
-                    move(0, 9),
-                    render_textured_unlit(
-                        game.MaterialTexturedUnlit,
+                    move(0, 5),
+                    render_textured_diffuse(
+                        game.MaterialTexturedDiffuse,
                         game.MeshCube,
-                        game.Textures["one"],
-                        GL_CW,
-                        [0.4, 0.65, 0.8, 1]
+                        game.Textures["missile"]
                     ),
                 ],
             },
