01781f694190742109b6e578bfc0a54cb30d02d5
Staś Małolepszy <stas@piesku.com>
2020-09-05 16:45:57 +0200

    Short missile sound

--- src/sounds/snd_missile.ts
+++ src/sounds/snd_missile.ts
@@ -3,9 +3,9 @@ import {AudioClip} from "../components/com_audio_source.js";
 export let snd_missile: AudioClip = {
     Tracks: [
         {
-            Instrument: [8, "lowpass", 9, 8, false, false, 8, 1, [[false, 8, 5, 18, 10]]],
+            Instrument: [8, "lowpass", 9, 8, false, false, 8, 1, [[false, 8, 5, 0, 9]]],
             Notes: [77],
         },
     ],
-    Exit: 99,
+    Exit: 0.1,
 };
