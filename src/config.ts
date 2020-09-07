03492b56b7dc7b3d2b01e8bd7edf43110d030221
Staś Małolepszy <stas@piesku.com>
2020-09-07 23:58:05 +0200

    Spawn missiles less freuqently

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ export const PLAYER_SPEED = 3;
 export const BUILDING_SPAWN_FREQUENCY = 0.4;
 export const BUILDING_SPAWN_Z = -20;
 export const BUILDING_LIFESPAN = Infinity;
-export const MISSILE_SPAWN_FREQUENCY = 5;
+export const MISSILE_SPAWN_FREQUENCY = 9;
 export const MISSILE_SPAWN_Z = -50;
