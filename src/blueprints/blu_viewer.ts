ce8feeb7f3e63cdc8918c78d683c1f4da846d0f8
Staś Małolepszy <stas@piesku.com>
2020-09-12 22:09:24 +0200

    ControlPoseKind

--- src/blueprints/blu_viewer.ts
+++ src/blueprints/blu_viewer.ts
@@ -3,7 +3,7 @@ import {audio_listener} from "../components/com_audio_listener.js";
 import {audio_source} from "../components/com_audio_source.js";
 import {camera_xr} from "../components/com_camera.js";
 import {collide} from "../components/com_collide.js";
-import {control_pose} from "../components/com_control_pose.js";
+import {ControlPoseKind, control_pose} from "../components/com_control_pose.js";
 import {control_spawn} from "../components/com_control_spawn.js";
 import {ControlXrKind, control_xr} from "../components/com_control_xr.js";
 import {emit_particles} from "../components/com_emit_particles.js";
@@ -33,7 +33,7 @@ export function blueprint_viewer(game: Game, scale: number): Blueprint {
                     {
                         // Head.
                         Translation: [0, 2, 0],
-                        Using: [control_pose("head"), named("head")],
+                        Using: [control_pose(ControlPoseKind.Head), named("head")],
                         Children: [
                             {
                                 // The head space has +Z towards the user, so we need to
@@ -95,7 +95,7 @@ export function blueprint_viewer(game: Game, scale: number): Blueprint {
                         // Left hand.
                         Translation: [-0.5, 2, 0],
                         Using: [
-                            control_pose("left"),
+                            control_pose(ControlPoseKind.Left),
                             control_xr(ControlXrKind.Left),
                             collide(true, Layer.PlayerHand, Layer.None, [0.1, 0.1, 0.1]),
                             rigid_body(RigidKind.Kinematic),
@@ -127,7 +127,7 @@ export function blueprint_viewer(game: Game, scale: number): Blueprint {
                         // Right hand.
                         Translation: [0.5, 2, 0],
                         Using: [
-                            control_pose("right"),
+                            control_pose(ControlPoseKind.Right),
                             control_xr(ControlXrKind.Right),
                             collide(true, Layer.PlayerHand, Layer.None, [0.1, 0.1, 0.1]),
                             rigid_body(RigidKind.Kinematic),
