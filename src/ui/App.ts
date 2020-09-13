fd9ebb8356e11233c03025370ae4d45d2c7ba065
Staś Małolepszy <stas@piesku.com>
2020-09-13 08:25:08 +0200

    switch in App

--- src/ui/App.ts
+++ src/ui/App.ts
@@ -4,48 +4,76 @@ import {Game} from "../game.js";
 import {EnterVr} from "./EnterVr.js";
 
 export function App(game: Game) {
-    if (game.CurrentStage === StageKind.Playing) {
-        return EnterVr(game);
+    switch (game.CurrentStage) {
+        case StageKind.Playing:
+            return EnterVr(game);
+        case StageKind.Intro:
+            return "";
+        case StageKind.Title:
+            return html`
+                <div
+                    style="
+                        position: absolute;
+                        top: 20vmin;
+                        left: 10vmin;
+                    "
+                >
+                    <div style="font-size: 20vmin">ROAR!</div>
+                    Baby-zilla has been taken away!
+                </div>
+                <div
+                    style="
+                        position: absolute;
+                        bottom: 10vmin;
+                        right: 10vmin;
+                    "
+                >
+                    <div onclick="$(${Action.GoToStage})">Play now</div>
+                </div>
+            `;
+        case StageKind.Clear:
+            return html`
+                <div
+                    style="
+                        position: absolute;
+                        top: 20vmin;
+                        left: 10vmin;
+                    "
+                >
+                    <div style="font-size: 20vmin">YOU WIN!</div>
+                    You've found it!
+                </div>
+                <div
+                    style="
+                        position: absolute;
+                        bottom: 10vmin;
+                        right: 10vmin;
+                    "
+                >
+                    <div onclick="$(${Action.GoToStage})">Play again</div>
+                </div>
+            `;
+        case StageKind.Failed:
+            return html`
+                <div
+                    style="
+                        position: absolute;
+                        top: 20vmin;
+                        left: 10vmin;
+                    "
+                >
+                    <div style="font-size: 20vmin">YOU LOSE!</div>
+                    A missile has killed you!
+                </div>
+                <div
+                    style="
+                        position: absolute;
+                        bottom: 10vmin;
+                        right: 10vmin;
+                    "
+                >
+                    <div onclick="$(${Action.GoToStage})">Play again</div>
+                </div>
+            `;
     }
-
-    if (game.CurrentStage === StageKind.Intro) {
-        return "";
-    }
-
-    return html`
-        <div
-            style="
-                position: absolute;
-                top: 20vmin;
-                left: 10vmin;
-            "
-        >
-            ${game.CurrentStage === StageKind.Title &&
-            `
-                <div style="font-size: 20vmin">ROAR!</div>
-                Baby-zilla has been taken away!
-            `}
-            ${game.CurrentStage === StageKind.Clear &&
-            `
-                <div style="font-size: 20vmin">YOU WIN!</div>
-                You've found it!
-            `}
-            ${game.CurrentStage === StageKind.Failed &&
-            `
-                <div style="font-size: 20vmin">YOU LOSE!</div>
-                A missile has killed you!
-            `}
-        </div>
-        <div
-            style="
-                position: absolute;
-                bottom: 10vmin;
-                right: 10vmin;
-            "
-        >
-            ${game.CurrentStage === StageKind.Title
-                ? `<div onclick="$(${Action.GoToStage})">Play now</div>`
-                : `<div onclick="$(${Action.GoToStage})">Play again</div>`}
-        </div>
-    `;
 }
