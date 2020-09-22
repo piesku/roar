b0be00aa4f635741c59f1cadec3d7dada073b21e
Staś Małolepszy <stas@piesku.com>
2020-09-22 19:10:36 +0200

    Better in-frustum checks on the Z axis

--- src/systems/sys_cull.ts
+++ src/systems/sys_cull.ts
@@ -37,7 +37,12 @@ function update(game: Game, entity: Entity) {
     if (
         Math.abs(ndc_pos[0]) > 1.1 ||
         Math.abs(ndc_pos[1]) > 1.1 ||
-        Math.abs(view_pos[2]) > camera.CullDistance
+        // The entity is behind the far plane of the camera frustum, which is actually
+        // behind the camera entity (-Z) since the camera looks backwards.
+        view_pos[2] < -camera.CullDistance ||
+        // The entity is behind the near plane of the camera frustum, i.e. in front
+        // of the camera entity (+Z).
+        view_pos[2] > 0
     ) {
         game.World.Signature[entity] &= ~cull.Mask;
     } else {
